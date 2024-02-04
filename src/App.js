3af51f57b3e12c113389a861e8735d3e6cddaf66
Nathaniel Mason <n11412402@qut.edu.au>
2024-02-05 09:21:44 +1000

    initialised working files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState } from "react";
+import PokemonList from "./PokemonList";
 
 function App() {
+  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
   return null;
 }
 
