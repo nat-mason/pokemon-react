25955f199ac985b7f11d6563fd3aa300c6080a11
Nathaniel Mason <n11412402@qut.edu.au>
2024-02-07 08:34:46 +1000

    init bootstrap

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -5,10 +5,23 @@ export default function PokemonList({ pokemon, pokeImage, shinyImage }) {
     <>
       <div>
         {pokemon.map((p, index) => (
-          <div key={p}>
-            <h1>{p}</h1>
-            <img src={pokeImage[index]} alt={pokemon} />
-            <img src={shinyImage[index]} alt="shiny" />
+          <div className="card" key={p}>
+            <img
+              src={pokeImage[index]}
+              className="card-img-top"
+              alt={pokemon}
+            />
+            <img src={shinyImage[index]} className="card-img-top" alt="shiny" />
+            <div className="card-body">
+              <h5 class="card-title">{p}</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
           </div>
         ))}
       </div>
