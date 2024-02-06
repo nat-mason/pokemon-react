1f2b252a42532798b4ecc31c1ebbf3b0820a0a10
Nathaniel Mason <n11412402@qut.edu.au>
2024-02-07 08:59:24 +1000

    put pokemon next to each other

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function PokemonList({ pokemon, pokeImage, shinyImage }) {
   return (
     <>
-      <div>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
         {pokemon.map((p, index) => (
-          <div className="card" style={{ width: 18 + "em" }} key={p}>
+          <div className="card" style={{ width: 10 + "rem" }} key={p}>
             <img
               src={pokeImage[index]}
               className="card-img-top"
@@ -13,12 +13,12 @@ export default function PokemonList({ pokemon, pokeImage, shinyImage }) {
             />
             <img src={shinyImage[index]} className="card-img-top" alt="shiny" />
             <div className="card-body">
-              <h5 class="card-title">{p}</h5>
-              <p class="card-text">
+              <h5 className="card-title">{p}</h5>
+              <p className="card-text">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#" class="btn btn-primary">
+              <a href="#" className="btn btn-primary">
                 Go somewhere
               </a>
             </div>
